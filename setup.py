c8384125192399582519c5c4d64013dff3673ba8
David H. <22520563+NewJerseyStyle@users.noreply.github.com>
2025-04-02 17:01:58 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='litepolis-middleware-template',                                  # Change
-    version="0.0.1",                                                       # Change
-    description='The middleware module for LitePolis',                     # Change
+    name='litepolis-middleware-rate-limiter',
+    version="0.0.1",
+    description='The rate limit middleware module for LitePolis',
     long_description=readme,
     long_description_content_type="text/markdown",
-    author='Your name',                                                    # Change
-    # author_email='Optional',                                             # Change
-    url='https://github.com/NewJerseyStyle/LitePolis-middleware-template', # Change
+    author='NewJerseyStyle',
+    # author_email='Optional',
+    url='https://github.com/NewJerseyStyle/LitePolis-middleware-rate-limiter',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=['litepolis', 'sqlmodel'],
+    install_requires=['litepolis'],
 )
